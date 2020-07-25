4232c577bb5a8c8ea585b8bcf03b04af94532b12
maxim00000maxim <63451409+maxim00000maxim@users.noreply.github.com>
2020-07-25 12:43:03 +0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,106 @@
+let u = 0
+let y = 0
+function setup() {
+  createCanvas(400, 400);
+  frameRate(3)
+  background(400);
+}
+
+function draw() {
+  
+  strokeWeight(20)
+  
+  
+
+  
+   u=random(0,399);
+  y=random(0,399);
+  fill(random(0,255),random(0,255),random(0,255),30)
+ellipse(y,u,u,y)
+  noFill()
+  u=random(0,399);
+  y=random(0,399);
+   stroke(random(0,255),random(0,255),random(0,255))
+  line(200,200,u,y)
+  u=random(0,399);
+  y=random(0,399)
+  
+
+  
+   u=random(0,399);
+  y=random(0,399);
+  fill(random(0,255),random(0,255),random(0,255),30)
+ellipse(y,u,u,y)
+  noFill()
+  u=random(0,399);
+  y=random(0,399);
+   stroke(random(0,255),random(0,255),random(0,255))
+  line(200,200,u,y)
+  u=random(0,399);
+  y=random(0,399)
+  
+
+  
+   u=random(0,399);
+  y=random(0,399);
+  fill(random(0,255),random(0,255),random(0,255),30)
+ellipse(y,u,u,y)
+  noFill()
+  u=random(0,399);
+  y=random(0,399);
+   stroke(random(0,255),random(0,255),random(0,255))
+  line(200,200,u,y)
+  u=random(0,399);
+  y=random(0,399)
+  
+
+  
+   u=random(0,399);
+  y=random(0,399);
+  fill(random(0,255),random(0,255),random(0,255),30)
+ellipse(y,u,u,y)
+  noFill()
+  u=random(0,399);
+  y=random(0,399);
+   stroke(random(0,255),random(0,255),random(0,255))
+  line(200,200,u,y)
+  u=random(0,399);
+  y=random(0,399)
+  
+
+  
+   u=random(0,399);
+  y=random(0,399);
+  fill(random(0,255),random(0,255),random(0,255),30)
+ellipse(y,u,u,y)
+  noFill()
+  u=random(0,399);
+  y=random(0,399);
+   stroke(random(0,255),random(0,255),random(0,255))
+  line(200,200,u,y)
+  u=random(0,399);
+  y=random(0,399)
+  
+
+  
+   u=random(0,399);
+  y=random(0,399);
+  fill(random(0,255),random(0,255),random(0,255),30)
+ellipse(y,u,u,y)
+  noFill()
+  u=random(0,399);
+  y=random(0,399);
+   stroke(random(0,255),random(0,255),random(0,255))
+  line(200,200,u,y)
+  u=random(0,399);
+  y=random(0,399)
+  
+
+  
+   u=random(0,399);
+  y=random(0,399);
+  fill(random(0,255),random(0,255),random(0,255),30)
+ellipse(y,u,u,y)
+  noFill()
+  
+}
